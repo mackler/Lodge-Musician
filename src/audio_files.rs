7da7ac42f72e15008ab58263e22d4e14c4db35e2
Adam Maklér <dev@upisom.sk>
2025-05-18 09:59:41 +0200

    Add some more buttons and music

--- src/audio_files.rs
+++ src/audio_files.rs
@@ -1,6 +1,8 @@
 pub const OPENING_PROCESSION: &str = "/home/mackler/Music/masonic-music/files-for-procession/opening-procession.wav";
+pub const OPEN_GREAT_LIGHTS: &str = "/home/mackler/Music/masonic-music/files-for-work/open-great-lights.wav";
 pub const TRACK2: &str = "audio2.mp3";
 pub const TRACK3: &str = "audio3.mp3";
 pub const RIMSHOT1: &str = "/home/mackler/Music/masonic-music/files-for-work/more-outro.wav";
-pub const EFFECT2: &str = "effect2.mp3";
-pub const EFFECT3: &str = "effect3.mp3";
+pub const RIMSHOT2: &str = "/home/mackler/Music/masonic-music/files-with-effects/c-major-hit-fattened.wav";
+pub const RIMSHOT3: &str = "/home/mackler/Music/masonic-music/files-with-effects/gliss-growl.wav";
+pub const RIMSHOT4: &str = "/home/mackler/Music/masonic-music/files-with-effects/going-down.wav";
--- src/main.rs
+++ src/main.rs
@@ -42,21 +42,49 @@ impl AudioToggle {
 fn main() -> Result<(), slint::PlatformError> {
     let (_stream, stream_handle) = OutputStream::try_default().expect("No audio output device");
 
-    let audio1 = Arc::new(Mutex::new(AudioToggle::new(OPENING_PROCESSION, stream_handle.clone())));
-    let rimshot1 = Arc::new(Mutex::new(AudioToggle::new(RIMSHOT1, stream_handle)));
+    let opening_procession = Arc::new(Mutex::new(AudioToggle::new(OPENING_PROCESSION, stream_handle.clone())));
+    let open_great_lights = Arc::new(Mutex::new(AudioToggle::new(OPEN_GREAT_LIGHTS, stream_handle.clone())));
+    let rimshot1 = Arc::new(Mutex::new(AudioToggle::new(RIMSHOT1, stream_handle.clone())));
+    let rimshot2 = Arc::new(Mutex::new(AudioToggle::new(RIMSHOT2, stream_handle.clone())));
+    let rimshot3 = Arc::new(Mutex::new(AudioToggle::new(RIMSHOT3, stream_handle.clone())));
+    let rimshot4 = Arc::new(Mutex::new(AudioToggle::new(RIMSHOT4, stream_handle.clone())));
 
     let main_window = MainWindow::new()?;
 
     {
-        let audio1 = audio1.clone();
+        let opening_procession = opening_procession.clone();
         main_window.on_toggle_opening_procession(move || {
-            audio1.lock().unwrap().toggle();
+            opening_procession.lock().unwrap().toggle();
+        });
+    }
+    {
+        let open_great_lights = open_great_lights.clone();
+        main_window.on_toggle_open_great_lights(move || {
+            open_great_lights.lock().unwrap().toggle();
         });
     }
     {
         let rimshot1 = rimshot1.clone();
         main_window.on_toggle_rimshot1(move || {
             rimshot1.lock().unwrap().toggle();
+        });
+    }
+    {
+        let rimshot2 = rimshot2.clone();
+        main_window.on_toggle_rimshot2(move || {
+            rimshot2.lock().unwrap().toggle();
+        });
+    }
+    {
+        let rimshot3 = rimshot3.clone();
+        main_window.on_toggle_rimshot3(move || {
+            rimshot3.lock().unwrap().toggle();
+        });
+    }
+    {
+        let rimshot4 = rimshot4.clone();
+        main_window.on_toggle_rimshot4(move || {
+            rimshot4.lock().unwrap().toggle();
         });
     }
 
